44b9721cbf18a08b1f6fc1c26913d33217a5c0e6
LeviNikhil <nikhilkumarjaiswal2946@gmail.com>
2024-07-14 02:32:35 +0530

    Added New Migration to add foreign key contraint in one to many relation

--- Flights Service/src/index.js
+++ Flights Service/src/index.js
@@ -10,6 +10,31 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async() => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+    const {Airport,City} = require('./models');
+    // const city = await City.findByPk(1);
+    // console.log('Cities:', city);
+//    const airport= Airport.create({
+//      name:'IndiraGandhiTerminal Airport',
+//      code:'IGTU',
+//      cityId:734,
+//    })
+   // console.log('Airport:', airport);
+//    const response= await city.createAirport({
+//     name:'IndiraGandhiTerminal Airport',
+//     code:'IGTU',
+//     cityId:'734',
+//     address:'New Delhi'
+//    });
+//    console.log('Airport:', response);
+
+    // const city = await City.findByPk(7);
+    // await city.createAirport({name:'Dublin Airport',code:'DUB',address:'Germany'});
+    //   await City.destroy({
+    //     where: {
+    //       id: 7,
+    //     },
+    //   });
+
 });
