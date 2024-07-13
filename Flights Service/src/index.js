88a7447f24a7b44e27b4d0fa36e431be0dd1bbaf
LeviNikhil <nikhilkumarjaiswal2946@gmail.com>
2024-07-14 04:05:07 +0530

    Added Flights

--- Flights Service/src/index.js
+++ Flights Service/src/index.js
@@ -10,31 +10,6 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, async() => {
-    console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
-    const {Airport,City} = require('./models');
-    // const city = await City.findByPk(1);
-    // console.log('Cities:', city);
-//    const airport= Airport.create({
-//      name:'IndiraGandhiTerminal Airport',
-//      code:'IGTU',
-//      cityId:734,
-//    })
-   // console.log('Airport:', airport);
-//    const response= await city.createAirport({
-//     name:'IndiraGandhiTerminal Airport',
-//     code:'IGTU',
-//     cityId:'734',
-//     address:'New Delhi'
-//    });
-//    console.log('Airport:', response);
-
-    // const city = await City.findByPk(7);
-    // await city.createAirport({name:'Dublin Airport',code:'DUB',address:'Germany'});
-    //   await City.destroy({
-    //     where: {
-    //       id: 7,
-    //     },
-    //   });
-
+app.listen(ServerConfig.PORT, () => {
+  console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
 });
